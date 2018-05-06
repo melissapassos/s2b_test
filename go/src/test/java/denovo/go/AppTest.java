a49d83907ea62464774e309aecf2a0f70fbb1b2d
Melissa Passos <melissa.padilha523@gmail.com>
2018-05-06 16:42:44 -0300

    teste parametrizado arquivo #3

--- go/src/test/java/denovo/go/AppTest.java
+++ go/src/test/java/denovo/go/AppTest.java
@@ -1,7 +1,7 @@
 package denovo.go;
 
 import org.junit.Test;
-//import org.junit.runner.RunWith;
+import org.junit.runner.RunWith;
 import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
 
